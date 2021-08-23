5d550d0192e55b562a9db22e5f90130c4aed4682
Brandon May <battleb2014@gmail.com>
2021-08-23 15:06:21 -0400

    base login/signup components built

--- potluck/src/components/Login.js
+++ potluck/src/components/Login.js
@@ -1,12 +1,25 @@
-import React from 'react'
+import React from 'react';
+import { Route, Link } from 'react-router-dom';
 
 function Login() {
     return (
         <div>
-            User login form
-            Username:
-            Password:
-            Don't have an account? Sign Up.
+            <h2>User login form</h2>
+            <label>Username:
+                <input  
+                    type = 'text'
+                    name = 'username'
+                    placeholder = 'Username'
+                />
+            </label>
+            <label>Password:
+                <input  
+                    type = 'text'
+                    name = 'password'
+                    placeholder = 'Password'
+                />
+            </label>
+            Don't have an account? <Link to = '/'>Sign Up</Link>.
         </div>
     )
 }
--- potluck/src/components/NewAccount.js
+++ potluck/src/components/NewAccount.js
@@ -3,9 +3,28 @@ import React from 'react'
 function NewAccount() {
     return (
         <div>
-            Sign up form
-            Username
-            Password
+            <h2>Sign up form</h2>
+            <label>Username:
+                <input  
+                    type = 'text'
+                    name = 'username'
+                    placeholder = 'Username'
+                />
+            </label>
+            <label>Password:
+                <input  
+                    type = 'text'
+                    name = 'password'
+                    placeholder = 'Password'
+                />
+            </label>
+            <label>Re-enter Password:
+                <input  
+                    type = 'text'
+                    name = 're-enter-password'
+                    placeholder = 'Re-enter Password'
+                />
+            </label>
             Role: Organizer or Guest
         </div>
     )

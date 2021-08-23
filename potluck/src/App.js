34408fac00c3e39a2e0eeb9bb86c143c5c17e25e
ashleyannlaz <paperpastels@gmail.com>
2021-08-23 14:03:09 -0400

    Added Router & Header Component

--- potluck/src/App.js
+++ potluck/src/App.js
@@ -1,10 +1,38 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Switch, Link} from "react-router-dom";
+
+import NewAccount from './components/NewAccount';
+import Login from './components/Login';
+import EditPotluck from './components/EditPotluck';
+import Potlucks from './components/Potlucks';
+import NewPotluck from './components/NewPotluck';
+import PotluckSignup from './components/PotluckSignup';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div className="App">
-      Hello, World.
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+      Hello, World. Links for easy building;
+        <div><Link to='/'>Home</Link></div>
+        <div><Link to='/new'>New Account</Link></div>
+        <div><Link to='/potlucks'>All Potlucks Info</Link></div>
+        <div><Link to='/addpotluck'>New Potluck</Link></div>
+        <div><Link to='/editpotluck'>Edit Potluck</Link></div>
+        <div><Link to='/potlucksignup'>Signup Potluck</Link></div>
+        
+        <Switch>
+            <Route path='/new' component={NewAccount} />
+            <Route path='/potlucks' component={Potlucks} />
+            <Route path='/addpotluck' component={NewPotluck} />
+            <Route path='/potlucksignup' component={PotluckSignup} />
+            <Route path='/editpotluck' component={EditPotluck} />
+            <Route path='/' component={Login} />
+            
+        </Switch>
+      </div>
+    </Router>
   );
 }
 

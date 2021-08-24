de652765a578b4fe738901bc0cade124d3564f04
Nathan <nathan.forman@gmail.com>
2021-08-24 12:22:26 -0700

    Added buttons to edit component

--- potluck/src/components/EditPotluck.js
+++ potluck/src/components/EditPotluck.js
@@ -2,11 +2,43 @@ import React from 'react'
 import './EditPotluck.css'
 
 function EditPotluck() {
+
+    let organizer = true;
+
+    const testPotlucks = [
+        {
+            id: 1,
+            name: 'Potluck Crew\'s Potluck',
+            date: 'August 23, 2021',
+            location: 'Orlando, FL',
+            food_items: ['Appetizer', 'Main Dish', 'Dessert']
+        },
+        {
+            id: 2,
+            name: 'Mabel\'s Birthday Potluck',
+            date: 'September 2, 2021',
+            location: 'Las Vegas, NV',
+            food_items: ['Appetizer', 'Main Dish']
+        }
+    ]
+
     return (
-        <div>
-            Organizers can edit potluck
+        <div className="form-container">
+            <h4>Edit Potluck</h4>
+            {testPotlucks.map(potluck => (
+            <div className='potluck'>
+                <h3>{potluck.name} </h3>
+                    <button>Edit Name</button>
+                <div className='potluck-info'>
+                    <p>Location: {potluck.location}</p>
+                        <button>Edit Location</button>
+                    <p>Date: {potluck.date}</p>
+                        <button>Edit Date</button>
+                </div>
+            </div>
+            ))}
         </div>
     )
 }
 
-export default EditPotluck
+export default EditPotluck;

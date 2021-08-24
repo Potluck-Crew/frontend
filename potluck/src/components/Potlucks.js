6cd6f166e6ce150635573094a92952e76c7713fb
Johnathan Figueroa <johnathan.c.figueroa@gmail.com>
2021-08-23 17:25:57 -0700

    Added listing potluck functionality/styling + add potluck link

--- potluck/src/components/Potlucks.js
+++ potluck/src/components/Potlucks.js
@@ -1,10 +1,42 @@
 import React from 'react'
 
+import './Potlucks.css';
+
 function Potlucks() {
+
+    let organizer = true;
+
+    // test potluck data
+    const testPotlucks = [
+        {   id: 1,
+            name: 'Potluck Crew\'s Potluck',
+            month: 'August',
+            date: 23, 
+            year: 2021
+        },
+        {   id: 2, 
+            name: 'Mabel\'s Birthday Potluck',
+            month: 'September',
+            date: 2,
+            year: 2021
+        }
+    ]
     return (
-        <div>
-            Here we see all of the potlucks available.
-            Organizers should see a link to create a new potluck.
+        <div class='container'>
+           <h2>Available Potlucks</h2>
+
+            {/*Looping through potlucks */}
+            {testPotlucks.map(potluck => (
+               <div className='potluck'> 
+                <h3>{potluck.name} </h3>
+                <div className='potluck-info'>
+                    <p>Date: {potluck.month} {potluck.date}, {potluck.year}</p>
+                </div>
+               </div>
+            ))}
+            {/*Showing add potluck button if organizer is true */}
+            {organizer && <a href='/addpotluck'> Add a potluck</a>}
+            
         </div>
     )
 }
